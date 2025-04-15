86f042a259c06c2ace25052d3185eab9b40252c7
giaccneto <giaccento@hotmail.com>
2025-04-14 20:25:45 -0400

    Commit inicial

---
+++ src/main/java/com/giaccneto/agendado_tarefas/AgendadoTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.giaccneto.agendado_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadoTarefasApplication.class, args);
+	}
+
+}
